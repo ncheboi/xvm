15bc44f9c95d7e7a1121a2e7d76ac738574f759f
Tyler <tyler@skotchpine.com>
2017-10-18 18:18:13 -0500

    test xvm core

--- source/xvm/xvm.go
+++ source/xvm/xvm.go
@@ -1,15 +1,24 @@
 package xvm
 
 import (
+	"fmt"
+	"io/ioutil"
+	"path/filepath"
+	"os"
+	"runtime"
+	"strings"
+
 	"./plugin"
 	"./group"
 )
 
 type Xvm struct {
-	Path, Platform string
+	Path     string // $XVMPATH, $USERPROFILE/xvm or $HOME/.xvm
+	Platform string // windows or unix
 }
 
-func StartXvm() (*Xvm, error) {
+// Creates a new Xvm instance.
+func StartXvm() *Xvm {
 	xvm := new(Xvm)
 
 	if runtime.GOOS == "windows" {
@@ -18,83 +27,89 @@ func StartXvm() (*Xvm, error) {
 		xvm.Platform = "unix"
 	}
 
-	xvm.Path, isSet := os.LookupEnv("XVMPATH")
-	if !isSet {
+	env, isSet := os.LookupEnv("XVMPATH")
+	if isSet {
+		xvm.Path = env
+	} else {
 		switch xvm.Platform {
 		case "windows":
-			xvm.Path = filepath.Join(os.Getenv("USERPROFILE"), ".xvm")
+			xvm.Path = filepath.Join(os.Getenv("USERPROFILE"), "xvm")
 		case "unix":
-			xvm.Path = filepath.Join(os.Getenv("Home"), ".xvm")
+			xvm.Path = filepath.Join(os.Getenv("HOME"), ".xvm")
 		}
 	}
 
 	return xvm
 }
 
-func (r *Run) GetNearestGroup(path string) *Group {
-	for path != "/" {
-		ind := filepath.Join(path, ".xvm")
+// Returns a group for the first directory containing a .xvm directory between
+// the path and the root or a group for the $XVMPATH.
+func (x *Xvm) GetNearestGroup(path string) *group.Group {
+	for path != "/" && path != x.Path {
+		groupPath := filepath.Join(path, ".xvm")
 
-		info, err := io.Stat(ind)
-		if err != nil && info.IsDir() {
-			return &Group{Path: path}
+		info, err := os.Stat(groupPath)
+		if err == nil && info.IsDir() {
+			return &group.Group{Path: groupPath}
 		}
 
-		path := path.Dir(path)
+		path = filepath.Dir(path)
 	}
 
-	return NewGroup(path.Dir(r.Path))
+	return &group.Group{Path: x.Path}
 }
 
-func (r *Run) PrintPluginsAvailable() error {
-	path := filepath.Join(r.Path, "available")
+// Get a map of plugins and their update urls from the $XVMPATH/available.
+func (x *Xvm) GetPluginsAvailable() (map[string]string, error) {
+	path := filepath.Join(x.Path, "available")
+
+	plugins := make(map[string]string)
 
 	content, err := ioutil.ReadFile(path)
-	if err == nil {
-		lines := strings.Split(content, "\n")
-		for _, line := range lines {
-			tokens := strings.Split(line, " ")
-			// TODO: fail before printing anything
-			if len(tokens) != 2 {
-				return fmt.Errorf("%s is ill-formatted", path)
-			}
-			fmt.Println(tokens[1])
+	if err != nil {
+		return nil, err
+	}
+
+	lines := strings.Split(string(content), "\n")
+	for _, line := range lines[:len(lines) - 1] {
+		tokens := strings.Split(line, " ")
+		if len(tokens) != 2 {
+			return nil, fmt.Errorf("%s is ill-formatted", path)
 		}
+
+		plugins[tokens[0]] = tokens[1]
 	}
-	return err
+
+	return plugins, nil
 }
 
-func (r *Run) PrintPluginsInstalled() ([]string, error) {
-	path := filepath.Join(r.Path, "installed")
-
-	names, err := ioutil.Readdirnames(path)
-	if err == nil {
-		fmt.Println(strings.Join(names, "\n"))
+// Get a list of all installed plugins from $XVMPATH/installed.
+func (x *Xvm) GetPluginsInstalled() ([]string, error) {
+	path := filepath.Join(x.Path, "installed")
+	dir, err := os.Open(path)
+	if err != nil {
+		return nil, err
 	}
-	return err
+	return dir.Readdirnames(0)
 }
 
-func (r *Run) GetPluginUrl(name string) {
-	path := filepath.Join(r.Path, "available")
-
-	content, err := ioutil.ReadFile(path)
-	if err == nil {
-		lines := strings.Split(content, "\n")
-		for _, line := range lines {
-			tokens := strings.Split(line, " ")
-			// TODO: fail unless well-formatted
-			if len(tokens) > 1 && tokens[0] == name {
-				return tokens[1]
-			}
-		}
+// Creates a Plugin for a given name.
+func (x *Xvm) GetPlugin(name string) (*plugin.Plugin, error) {
+	available, err := x.GetPluginsAvailable()
+	if err != nil {
+		return nil, err
 	}
-	return err
-}
 
-func (r *Run) GetPlugin(name string) *Plugin {
-	plugin := new(Plugin)
+	url, ok := available[name]
+	if !ok {
+		return nil, fmt.Errorf("Plugin not found: %s", name)
+	}
 
-	plugin.Path = filepath.Join(r.Path, "installed", name)
-	plugin.Bin = filepath.Join(r.Path, "bin." + r.Platform, name)
-	plugin.Url = GetPluginUrl(name)
+	p := &plugin.Plugin{
+		Path: filepath.Join(x.Path, "installed", name),
+		Bin:  filepath.Join(x.Path, "bin." + x.Platform, name),
+		Url:  url,
+	}
+
+	return p, nil
 }

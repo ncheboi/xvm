2b11c2aec115466774395e370543ed0bcf1012a9
Tyler <tyler@skotchpine.com>
2017-10-19 18:24:13 -0500

    successfully pull plugins

--- xvm.go
+++ xvm.go
@@ -11,6 +11,7 @@ import (
 )
 
 var (
+	platform string
 	xvmName string
 	xvmHome string
 	osHome  string
@@ -18,6 +19,25 @@ var (
 	local   string
 	pwd     string
 )
+
+type Cmd struct {
+	min, max int
+	fn       func()
+}
+
+func (cmd *Cmd) run() {
+	if len(os.Args) < cmd.min {
+		printGlobalFile("usage")
+		os.Exit(1)
+	}
+
+	if len(os.Args) > cmd.max {
+		printGlobalFile("usage")
+		os.Exit(1)
+	}
+
+	cmd.fn()
+}
 
 func fail(msg string, a ...interface{}) {
 	fmt.Fprintf(os.Stderr, msg+"\n", a...)
@@ -111,27 +131,13 @@ func printFile(path string) {
 	fmt.Print(string(contents))
 }
 
-func argMin(min int) {
-	if len(os.Args) < min {
-		printGlobalFile("usage")
-		os.Exit(1)
-	}
-}
-
-func argMax(max int) {
-	if len(os.Args) > max {
-		printGlobalFile("usage")
-		os.Exit(1)
-	}
-}
-
-type Cmd func(args ...string)
-
 func init() {
 	if runtime.GOOS == "windows" {
+		platform = "windows"
 		xvmName = "xvm"
 		osHome = os.Getenv("USERPROFILE")
 	} else {
+		platform = "unix"
 		xvmName = ".xvm"
 		osHome = os.Getenv("HOME")
 	}
@@ -152,7 +158,12 @@ func init() {
 
 	x := pwd
 	for x != "/" {
-		if x == xvmHome || isDir(xvmPath(x)) {
+		if x == xvmHome {
+			local = global
+			break
+		}
+
+		if isDir(xvmPath(x)) {
 			local = xvmPath(x)
 			break
 		}
@@ -165,205 +176,195 @@ func init() {
 	}
 }
 
+var cmds = map[string]Cmd{
+	"version": {2, 2,
+		func() {
+			printGlobalFile("version")
+		},
+	},
+
+	"usage": {2, 2,
+		func() {
+			printGlobalFile("README.md")
+		},
+	},
+
+	"help": {2, 2,
+		func() {
+			printGlobalFile("help")
+		},
+	},
+
+	"init": {2, 2,
+		func() {
+			if local == xvmPath(pwd) {
+				fail("Group already exists: %s", local)
+			}
+			mkdirAll(filepath.Join(xvmPath(pwd), "versions"))
+		},
+	},
+
+	"which": {2, 2,
+		func() {
+			fmt.Println(local)
+		},
+	},
+
+	"status": {2, 3,
+		func() {
+			which := local
+
+			if len(os.Args) > 2 {
+				if os.Args[2] == "global" {
+					which = global
+				} else if os.Args[2] != "local" {
+					fail("Unknown argument: %s", os.Args[2])
+				}
+			}
+
+			versionsPath := filepath.Join(which, "versions")
+			if !isDir(versionsPath) {
+				mkdirAll(versionsPath)
+			}
+			plugins := dirNames(versionsPath)
+
+			for _, plugin := range plugins {
+				printFile(filepath.Join(versionsPath, plugin))
+			}
+		},
+	},
+
+	"remove": {2, 2,
+		func() {
+			removeAll(local)
+		},
+	},
+
+	"list": {3, 4,
+		func() {
+			p := pluginPath(os.Args[2])
+			ensurePlugin(p)
+
+			which := "installed"
+
+			if len(os.Args) > 3 {
+				if os.Args[3] == "available" {
+					which = "available"
+				} else if os.Args[3] != "installed" {
+					fail("Unknown argument: %s", os.Args[3])
+				}
+			}
+
+			path := filepath.Join(p, which)
+			if isDir(path) {
+				versions := dirNames(path)
+				fmt.Println(strings.Join(versions, "\n"))
+			}
+		},
+	},
+
+	"pull": {3, 4,
+		func() {
+			plugin := os.Args[2]
+			version := os.Args[3]
+
+			p := pluginPath(plugin)
+			ensurePlugin(p)
+
+			destDir := installedPath(p, version)
+
+			contentPath := filepath.Join(p, "available", version)
+			rawContent, err := ioutil.ReadFile(contentPath)
+			failIf(err)
+
+			content := strings.TrimRight(string(rawContent), "\n")
+
+			bin := filepath.Join(p, platform, "pull")
+
+			os.Setenv("XVM_PULL_DESTDIR", destDir)
+			os.Setenv("XVM_PULL_VERSION", version)
+			os.Setenv("XVM_PULL_CONTENT", content)
+
+			cmd := exec.Command(bin)
+			cmd.Stdout = os.Stdout
+			cmd.Stderr = os.Stderr
+
+			if err := cmd.Run(); err != nil {
+				fail("%s", err)
+			}
+		},
+	},
+
+	"drop": {4, 4,
+		func() {
+			p := pluginPath(os.Args[2])
+			ensurePlugin(p)
+
+			i := installedPath(p, os.Args[3])
+			ensureInstalled(i)
+
+			removeAll(i)
+		},
+	},
+
+	"set": {4, 5,
+		func() {
+			which := local
+			plugin := os.Args[2]
+			version := os.Args[3]
+
+			if len(os.Args) > 4 {
+				if os.Args[2] == "global" {
+					which = global
+				} else if os.Args[2] != "local" {
+					fail("Unknown argument: %s", os.Args[2])
+				}
+
+				plugin = os.Args[3]
+				version = os.Args[4]
+			}
+
+			p := pluginPath(plugin)
+			ensurePlugin(p)
+
+			i := installedPath(p, version)
+			ensureInstalled(i)
+
+			path := filepath.Join(which, "versions", plugin)
+			writeFile(path, filepath.Base(i)+"\n")
+		},
+	},
+
+	"unset": {3, 4,
+		func() {
+			which := local
+			plugin := os.Args[3]
+
+			if len(os.Args) > 3 {
+				if os.Args[2] == "global" {
+					which = global
+				} else if os.Args[2] != "local" {
+					fail("Unknown argument: %s", os.Args[2])
+				}
+			} else {
+				plugin = os.Args[2]
+			}
+
+			ensurePlugin(pluginPath(plugin))
+			removeAll(filepath.Join(which, "versions", plugin))
+		},
+	},
+}
+
 func main() {
-	argMin(2)
+	if len(os.Args) < 2 {
+		printGlobalFile("usage")
+		os.Exit(1)
+	}
 
-	cmd, ok := map[string]Cmd{
-		"version": versionCmd,
-		"usage":   usageCmd,
-		"help":    helpCmd,
-		"init":    initCmd,
-		"which":   whichCmd,
-		"status":  statusCmd,
-		"remove":  removeCmd,
-		"list":    listCmd,
-		"pull":    pullCmd,
-		"drop":    dropCmd,
-		"set":     setCmd,
-		"unset":   unsetCmd,
-	}[os.Args[1]]
-
+	cmd, ok := cmds[os.Args[1]]
 	if !ok {
 		fail("Unknown command: %s", os.Args[1])
 	}
-
-	cmd()
-}
-
-func versionCmd(args ...string) {
-	argMin(2)
-	argMax(2)
-
-	printGlobalFile("version")
-}
-
-func usageCmd(args ...string) {
-	argMin(2)
-	argMax(2)
-
-	printGlobalFile("usage")
-}
-
-func helpCmd(args ...string) {
-	argMin(2)
-	argMax(2)
-
-	printGlobalFile("README.md")
-}
-
-func initCmd(args ...string) {
-	argMin(2)
-	argMax(2)
-
-	if local == xvmPath(pwd) {
-		fail("Group already exists: %s", local)
-	}
-
-	mkdirAll(filepath.Join(xvmPath(pwd), "versions"))
-}
-
-func whichCmd(args ...string) {
-	argMin(2)
-	argMax(2)
-
-	fmt.Println(local)
-}
-
-func statusCmd(args ...string) {
-	argMin(2)
-	argMax(3)
-
-	which := local
-
-	if len(os.Args) > 2 {
-		if os.Args[2] == "global" {
-			which = global
-		} else if os.Args[2] != "local" {
-			fail("Unknown argument: %s", os.Args[2])
-		}
-	}
-
-	versionsPath := filepath.Join(which, "versions")
-	if !isDir(versionsPath) {
-		mkdirAll(versionsPath)
-	}
-	plugins := dirNames(versionsPath)
-
-	for _, plugin := range plugins {
-		printFile(filepath.Join(versionsPath, plugin))
-	}
-}
-
-func removeCmd(args ...string) {
-	argMin(2)
-	argMax(2)
-
-	removeAll(local)
-}
-
-func listCmd(args ...string) {
-	argMin(3)
-	argMax(4)
-
-	p := pluginPath(os.Args[2])
-	ensurePlugin(p)
-
-	showAvailable := false
-
-	if len(os.Args) > 3 {
-		if os.Args[3] == "available" {
-			showAvailable = true
-		} else if os.Args[3] != "installed" {
-			fail("Unknown argument: %s", os.Args[3])
-		}
-	}
-
-	if showAvailable {
-		printFile(filepath.Join(p, "available"))
-		return
-	}
-
-	versions := dirNames(filepath.Join(p, "installed"))
-	fmt.Println(strings.Join(versions, "\n"))
-}
-
-func pullCmd(args ...string) {
-	argMin(3)
-	argMax(4)
-
-	p := pluginPath(os.Args[2])
-	ensurePlugin(p)
-
-	bin := filepath.Join(p, "bin", "pull")
-
-	cmd := exec.Command(bin, os.Args[3])
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-
-	if err := cmd.Run(); err != nil {
-		fail("%s", err)
-	}
-}
-
-func dropCmd(args ...string) {
-	argMin(4)
-	argMax(4)
-
-	p := pluginPath(os.Args[2])
-	ensurePlugin(p)
-
-	i := installedPath(p, os.Args[3])
-	ensureInstalled(i)
-
-	removeAll(i)
-}
-
-func setCmd(args ...string) {
-	argMin(4)
-	argMax(5)
-
-	which := local
-	plugin := os.Args[2]
-	version := os.Args[3]
-
-	if len(os.Args) > 4 {
-		if os.Args[2] == "global" {
-			which = global
-		} else if os.Args[2] != "local" {
-			fail("Unknown argument: %s", os.Args[2])
-		}
-
-		plugin = os.Args[3]
-		version = os.Args[4]
-	}
-
-	p := pluginPath(plugin)
-	ensurePlugin(p)
-
-	i := installedPath(p, version)
-	ensureInstalled(i)
-
-	path := filepath.Join(which, "versions", plugin)
-	writeFile(path, filepath.Base(i)+"\n")
-}
-
-func unsetCmd(args ...string) {
-	argMin(3)
-	argMax(4)
-
-	which := local
-	plugin := os.Args[3]
-
-	if len(os.Args) > 3 {
-		if os.Args[2] == "global" {
-			which = global
-		} else if os.Args[2] != "local" {
-			fail("Unknown argument: %s", os.Args[2])
-		}
-	} else {
-		plugin = os.Args[2]
-	}
-
-	ensurePlugin(pluginPath(plugin))
-	removeAll(filepath.Join(which, "versions", plugin))
+	cmd.run()
 }
